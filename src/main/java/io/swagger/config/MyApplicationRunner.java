98045969c43d2a34c14cde77d5abb30a525f1b53
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2022-05-19 20:25:09 +0200

    other commits

--- src/main/java/io/swagger/config/MyApplicationRunner.java
+++ src/main/java/io/swagger/config/MyApplicationRunner.java
@@ -10,6 +10,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
@@ -27,13 +28,35 @@ public class MyApplicationRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception{
-        LocalDateTime startDate = LocalDateTime.of(2022, 02, 13, 17, 48, 33);
+
         List<User> users = List.of(new User("Biniam","Tommy@","customer",1000.00,500.00,0.00));
-        LocalDateTime startDate1 = LocalDateTime.of(2022, 02, 13, 18, 15, 33);
+
         // Integer userPerformingId, String fromAccount, String toAccount, Double amount, String transactionType, java.time.LocalDateTime timestamp, Double balanceAfterTransfer
+        String str = "2018-12-10 12:30";
+        String str2 = "2018-12-11 13:30";
+        String str3 = "2018-12-12 14:30";
+        String str4 = "2018-12-13 15:30";
+        String str5 = "2018-12-14 16:30";
+        String str6 = "2018-12-15 17:30";
+        String str7 = "2018-12-16 18:30";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime dateTime =  LocalDateTime.parse(str, formatter);
+        LocalDateTime dateTime2 = LocalDateTime.parse(str2, formatter);
+        LocalDateTime dateTime3 = LocalDateTime.parse(str3, formatter);
+        LocalDateTime dateTime4 =  LocalDateTime.parse(str4, formatter);
+        LocalDateTime dateTime5 = LocalDateTime.parse(str5, formatter);
+        LocalDateTime dateTime6 = LocalDateTime.parse(str6, formatter);
+        LocalDateTime dateTime7 =  LocalDateTime.parse(str7, formatter);
+
+
         List<Transaction> transactions = List.of(
-            new Transaction(1, "jjsdbjkbsdjbvkjsdn", "sjbdvkjsdvskdvn", 2000.00, "bank type", startDate, 500.00),
-            new Transaction(1, "jjsdbjkbsdjbvkjsdn", "sjbdvkjsdvskdvn", 2000.00, "bank type", startDate1, 500.00)
+            new Transaction(1, "jjsdbjkbsdjbvkjsdn", "sjbdvkjsdvskdvn", 2000.00, "bank type", dateTime, 500.00),
+            new Transaction(2, "jjsdbjkbsdjbvkjsdn", "sjbdvkjsdvskdvn", 2000.00, "bank type", dateTime2, 500.00),
+                new Transaction(1, "jjsdbjkbsdjbvkjsdn", "sjbdvkjsdvskdvn", 2000.00, "bank type", dateTime3, 500.00),
+                new Transaction(2, "jjsdbjkbsdjbvkjsdn", "sjbdvkjsdvskdvn", 2000.00, "bank type", dateTime4, 500.00),
+                new Transaction(1, "jjsdbjkbsdjbvkjsdn", "sjbdvkjsdvskdvn", 2000.00, "bank type", dateTime5, 500.00),
+                new Transaction(2, "jjsdbjkbsdjbvkjsdn", "sjbdvkjsdvskdvn", 2000.00, "bank type", dateTime6, 500.00),
+                new Transaction(1, "jjsdbjkbsdjbvkjsdn", "sjbdvkjsdvskdvn", 2000.00, "bank type", dateTime7, 500.00)
         );
         userRepository.saveAll(users);
         transactionRepository.saveAll(transactions);
--- src/main/java/io/swagger/repository/TransactionRepository.java
+++ src/main/java/io/swagger/repository/TransactionRepository.java
@@ -1,7 +1,9 @@
 package io.swagger.repository;
 
 import io.swagger.model.Transaction;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -9,5 +11,10 @@ import java.util.Date;
 import java.util.List;
 
 public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
-    List<Transaction> findBytimestampCreatedBetween(LocalDateTime startDate, LocalDateTime endDate);
+    Iterable<Transaction> getTransactionByTimestamp(LocalDateTime timestamp);
+
+    @Query(value = "SELECT transactionId, transactionType, balanceAfterTransfer, userPerformingId  FROM Transaction WHERE timestamp BETWEEN :startDate AND :endDate")
+    public Iterable<Transaction> getAllTransactionsBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+
+    Iterable<Transaction> getTransactionByFromAccount(String fromAccount);
 }
